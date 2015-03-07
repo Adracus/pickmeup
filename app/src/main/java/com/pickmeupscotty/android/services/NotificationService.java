dca0254dc9d8b2ef2a3b0c7dc3dc8b5e5be9746e
flower-pot <flower-pot@flowerpot.io>
2015-03-07 23:34:48 +0100

    add attributes to build pick up request to intent

--- app/src/main/java/com/pickmeupscotty/android/services/NotificationService.java
+++ app/src/main/java/com/pickmeupscotty/android/services/NotificationService.java
@@ -9,7 +9,6 @@ import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 import com.pickmeupscotty.android.R;
-import com.pickmeupscotty.android.activities.DriverActivity;
 import com.pickmeupscotty.android.activities.ResponseActivity;
 import com.pickmeupscotty.android.amqp.RabbitService;
 import com.pickmeupscotty.android.amqp.Subscriber;
@@ -34,42 +33,36 @@ public class NotificationService extends IntentService {
         RabbitService.subscribe(PickUpRequest.class, new Subscriber<PickUpRequest>() {
             @Override
             public void on(PickUpRequest request) {
-
                 Context context = getApplicationContext();
-
 
                 // berechne ob notification angezeigt werden soll.
 
                 Intent notificationIntent = new Intent(context, ResponseActivity.class);
-//                notificationIntent.putExtra("FACEBOOK_ID", request.facebookid);
-                notificationIntent.putExtra("FACEBOOK_ID", "facebookid");
+                notificationIntent.putExtra(PickUpRequest.FACEBOOK_ID, request.getFacebookId());
+                notificationIntent.putExtra(PickUpRequest.CURRENT_LATITUDE, request.getCurrentLatitude());
+                notificationIntent.putExtra(PickUpRequest.CURRENT_LONGITUDE, request.getCurrentLongitude());
+                notificationIntent.putExtra(PickUpRequest.DESTINATION_LATITUDE, request.getDestinationLatitude());
+                notificationIntent.putExtra(PickUpRequest.DESTINATION_LONGITUDE, request.getDestinationLongitude());
 
                 PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
 
                 NotificationCompat.Builder notification = new NotificationCompat.Builder(getApplicationContext())
-                        .setContentTitle("Test")
-                        .setContentText("Test")
+                        .setContentTitle("Pick Up Request")
+                        .setContentText("by " + request.getFacebookId())
                         .setSmallIcon(R.drawable.notification);
                 notification = notification
                         .setContentIntent(contentIntent);
-
 
                 NotificationManager mNotificationManager =
                         (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
                 mNotificationManager.notify(12, notification.build());
 
-                int duration = Toast.LENGTH_SHORT;
-
-                Toast toast = Toast.makeText(getApplicationContext(), request.toString(), duration);
-                toast.show();
-
-
+                Toast.makeText(getApplicationContext(), request.toString(), Toast.LENGTH_SHORT).show();
             }
         });
 
         while (true) {
-                Thread.yield();
+            Thread.yield();
         }
-
     }
 }
