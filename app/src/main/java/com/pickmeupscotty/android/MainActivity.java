f7beafb6741aa9d2ea601d1c4e2d115d0e5cf0d5
Florian Loch <florian.loch@gmail.com>
2015-03-07 21:28:01 +0100

    Add icons, move login fragment to main activity

--- app/src/main/java/com/pickmeupscotty/android/MainActivity.java
+++ app/src/main/java/com/pickmeupscotty/android/MainActivity.java
@@ -3,27 +3,64 @@ package com.pickmeupscotty.android;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.facebook.SessionState;
 import com.pickmeupscotty.android.activities.DriverActivity;
 
 import com.pickmeupscotty.android.activities.PickMeUp;
 import com.pickmeupscotty.android.amqp.RabbitService;
+import com.pickmeupscotty.android.login.LoginFragment;
 import com.pickmeupscotty.android.services.NotificationService;
 import com.pickmeupscotty.android.login.FBWrapper;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
     private static final String TAG = MainActivity.class.getName();
+
+    private LoginFragment loginFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            // Add the fragment on initial activity setup
+            loginFragment = new LoginFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.login_fragment, loginFragment)
+                    .commit();
+        } else {
+            // Or set the fragment from restored state info
+            loginFragment = (LoginFragment) getSupportFragmentManager().findFragmentById(R.id.login_fragment);
+        }
+
+        final Button drivingButton = (Button) findViewById(R.id.drivingButton);
+        final Button pickMeUpButton = (Button) findViewById(R.id.pickMeUpButton);
+
+        FBWrapper.INSTANCE.addFacebookLoginOpenedListener(new FBWrapper.FacebookLoginStateListener() {
+            @Override
+            public void onStateChanged() {
+                drivingButton.setVisibility(Button.VISIBLE);
+                pickMeUpButton.setVisibility(Button.VISIBLE);
+            }
+        });
+        FBWrapper.INSTANCE.addFacebookLoginClosedListener(new FBWrapper.FacebookLoginStateListener() {
+            @Override
+            public void onStateChanged() {
+                drivingButton.setVisibility(Button.INVISIBLE);
+                pickMeUpButton.setVisibility(Button.INVISIBLE);
+            }
+        });
+
 
         RabbitService.create("facebookid");
         FBWrapper.INSTANCE.addFacebookLoginOpenedListener(new FBWrapper.FacebookLoginStateListener() {
